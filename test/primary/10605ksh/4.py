57cd881c1f5491bf3c2d2eb6442af2caec56f4ea
CharlesbrownK <ekjh0418@gmail.com>
2022-06-15 02:30:35 +0900

    add new test direction

---
+++ test/primary/10605ksh/4.py
@@ -0,0 +1,12 @@
+icecream = icecream = {'탱크보이': '1200', '폴라포': '1200', '빵빠레': '1800', '월드콘': '1500', '메로나': '1000'}
+a = icecream['탱크보이']
+b = icecream['폴라포']
+c = icecream['빵빠레']
+d = icecream['월드콘']
+e = icecream['메로나']
+A = int(a)
+B = int(b)
+C = int(c)
+D = int(d)
+E = int(e)
+total = (A + B + C + D + E)
---
+++ test/primary/10605ksh/5.py
@@ -0,0 +1,31 @@
+#4번 코드입니다.
+icecream = icecream = {'탱크보이': '1200', '폴라포': '1200', '빵빠레': '1800', '월드콘': '1500', '메로나': '1000'}
+a = icecream['탱크보이']
+b = icecream['폴라포']
+c = icecream['빵빠레']
+d = icecream['월드콘']
+e = icecream['메로나']
+A = int(a)
+B = int(b)
+C = int(c)
+D = int(d)
+E = int(e)
+total = (A + B + C + D + E)
+#여기서부터 5번입니다.
+sell = {'탱크보이': '10', '폴라포': '20', '빵빠레': '17', '월드콘': '9', '메로나': '37'}
+f = sell['탱크보이']
+g = sell['폴라포']
+h = sell['빵빠레']
+i = sell['월드콘']
+j = sell['메로나']
+F = int(f)
+G = int(g)
+H = int(h)
+I = int(i)
+J = int(j)
+v = A * F
+w = B * G
+x = C * H
+y = D * I
+z = E * J
+total2 = (v + w + x + y + z)
