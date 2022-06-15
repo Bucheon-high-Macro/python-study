a4138ddb5af735d9f8bfdb98e29225b8fc29c0e5
noobcommand <103097588+noobcommand@users.noreply.github.com>
2022-06-15 14:55:31 +0900

    Update 4.py

---
+++ test/primary/noobcommand/4.py
@@ -0,0 +1,7 @@
+icecream = {'탱크보이':'1200', '폴라포':'1200', '빵빠레':'1800', '월드콘':'1500', '메로나':'1000'}
+a = int(icecream.get('탱크보이'))
+b = int(icecream.get('폴라포'))
+c = int(icecream.get('빵빠레'))
+d = int(icecream.get('월드콘'))
+e = int(icecream.get('메로나'))
+print(a+b+c+d+e)
