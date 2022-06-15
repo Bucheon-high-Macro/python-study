7ad32eae45f6197a01924a61f9c9cb1ad9275d65
Junghoon Kim <86881143+CharlesbrownK@users.noreply.github.com>
2022-06-15 14:29:28 +0900

    Upload 4.py code

---
+++ test/secondary/CharlesbrownK/4.py
@@ -0,0 +1,11 @@
+toy = {"레고":'12000', '기차':'15000', '로봇':'9000', '지구본':'13500', '시계':'10000'}
+
+lego = int(toy["레고"])
+train = int(toy["기차"])
+robot = int(toy["로봇"])
+globe = int(toy["지구본"])
+watch = int(toy["시계"])
+
+result = lego + train + robot + globe + watch
+
+print(result)
