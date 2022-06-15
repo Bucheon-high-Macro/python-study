c8a8feddc221d01e1625f5fa5a78c7faf5b910b4
noobcommand <103097588+noobcommand@users.noreply.github.com>
2022-06-15 14:52:38 +0900

    Update 4.py

---
+++ test/secondary/noobcommand/4.py
@@ -0,0 +1,7 @@
+toy = {'레고':'12000', '기차':'15000', '로봇':'9000', '지구본':'13500', '시계':'10000'}
+a = int(toy.get('레고'))
+b = int(toy.get('기차'))
+c = int(toy.get('로봇'))
+d = int(toy.get('지구본'))
+e = int(toy.get('시계'))
+print(a+b+c+d+e)
